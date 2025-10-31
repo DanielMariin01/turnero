5028dba6e26112b1e5f6f11ed97dd1f484c3f499
ANALISTA.TI3 - Daniel Stiven Marin <ANALISTA.TI3@CLINICACE.LOCAL>
2025-10-31 15:08:10 -0500

    primer commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+            <h1>¡Hola Componente de clinica central del eje! 🚀</h1>
+        </div>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
